667c5e27476bc967c961d829a9ff6206ff6d73d6
Davyd <davydkrysiuk@gmail.com>
2025-08-15 06:34:19 +0100

    Bug fixes, added image scale

--- src/automaton.rs
+++ src/automaton.rs
@@ -18,7 +18,7 @@ impl Automaton {
             conditions: vec![]
         }
     }
-
+    #[allow(dead_code)]
     pub fn display(&self) {
         for i in 0..self.grid.height {
             for j in 0..self.grid.width {
@@ -33,14 +33,11 @@ impl Automaton {
     }
 
     pub fn add_condition(&mut self, amount_to_switch: usize, condition_state: State, initial_state: State, result_state: State) {
-        let condition = Condition {
-            amount_to_switch, condition_state, initial_state, result_state
-        };
-        self.conditions.push(condition);
+        self.conditions.push(Condition::new(amount_to_switch, initial_state, result_state, condition_state));
     }
 
     pub fn add_condition_range(&mut self, from: usize, until: usize, condition_state: State, initial_state: State, result_state: State) {
-        for i in from..until {
+        for i in from..=until {
             self.add_condition(i, condition_state, initial_state, result_state);
         }
     }
@@ -72,16 +69,25 @@ impl Automaton {
         self.grid = updated_grid;
     }
 
-    pub fn produce_image(&self, filename: &str) -> Result<(), image::ImageError> {
+    pub fn produce_image(&self, scale: usize, filename: &str) -> Result<(), image::ImageError> {
         let mut img: ImageBuffer<Rgb<u8>, Vec<u8>>= ImageBuffer::new(self.grid.width as u32, self.grid.height as u32);
 
-        for (x, y, pixel) in img.enumerate_pixels_mut() {
-            let state = self.grid.return_at_pos(x as usize, y as usize);
-            let color = match state {
-              Live => Rgb([255, 0, 0]),
-              Dead => Rgb([0, 0, 0])
-            };
-            *pixel = color;
+        for x in 0..self.grid.width / scale {
+            for y in 0..self.grid.height / scale {
+                let state = self.grid.return_at_pos(x, y);
+                let color = match state {
+                  Live => Rgb([255, 0, 0]),
+                  Dead => Rgb([0, 0, 0])
+                };
+
+                for i in 0..scale {
+                    for j in 0..scale {
+                        let pixel_x = x * scale + i;
+                        let pixel_y = y * scale + j;
+                        img.put_pixel(pixel_x as u32, pixel_y as u32, color);
+                    }
+                }
+            }
         }
         img.save(filename)
     }
--- src/main.rs
+++ src/main.rs
@@ -18,24 +18,36 @@ fn main() {
     let (width, height) = (1280, 720);
     let mut automaton: Automaton = Automaton::new(height, width);
 
-    automaton.add_condition(0, Live, Live, Dead);
-    automaton.add_condition(1, Live, Live, Dead);
-    automaton.add_condition(2, Live, Live, Live);
-    automaton.add_condition(3, Live, Live, Live);
-    automaton.add_condition_range(4, 8, Live, Live, Dead);
-    automaton.add_condition(3, Live, Dead, Live);
-    automaton.grid.randomize();
+    // Conway's Game of Life
+    // automaton.add_condition(0, Live, Live, Dead);
+    // automaton.add_condition(1, Live, Live, Dead);
+    // automaton.add_condition(2, Live, Live, Live);
+    // automaton.add_condition(3, Live, Live, Live);
+    // automaton.add_condition_range(4, 7, Live, Live, Dead);
+    // automaton.add_condition(3, Live, Dead, Live);
 
+    automaton.add_condition(3, Live, Dead, Live);
+    automaton.add_condition(6, Live, Dead, Live);
+    automaton.add_condition(7, Live, Dead, Live);
+    automaton.add_condition(8, Live, Dead, Live);
+    automaton.add_condition(3, Live, Live, Live);
+    automaton.add_condition(4, Live, Live, Live);
+    automaton.add_condition(6, Live, Live, Live);
+    automaton.add_condition(7, Live, Live, Live);
+    automaton.add_condition(8, Live, Live, Live);
+
+    automaton.grid.randomize();
     let mut counter = 0;
+    let limit = 1000;
     let start_time = Instant::now();
     loop {
-        if counter == 1000 {
+        if counter >= limit {
             break;
         }
         counter += 1;
         automaton.update(moore);
-        println!("workin");
-        automaton.produce_image(format!("./images/{}.png", counter).as_str() ).expect("Failed drawing");
+        println!("{}/{}", counter, limit);
+        automaton.produce_image(1, format!("./images/{}.png", counter).as_str() ).expect("Failed drawing");
     }
     let duration = start_time.elapsed();
     println!("Time taken: {:?}", duration);
